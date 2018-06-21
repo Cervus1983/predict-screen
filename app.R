0a3220556ca0e1e613a4127ad71dfb71086fb0ef
Mikhail Zhilkin <mikhail.zhilkin@kambi.com>
2018-06-21 12:28:18 +0200

    .

--- app.R
+++ app.R
@@ -1,19 +1,26 @@
 library(extrafont)
-library(ggthemes)
+#library(ggthemes)
 library(MASS)
 library(plotly)
 library(shiny)
 library(tidyverse)
 
-ui <- fluidPage(
-	plotlyOutput("heatmap"),
+ui <- fillPage(
+	h2("Adjusted R squared"),
+	fillRow(
+		plotlyOutput("heatmap", height = "100%"),
+		height = "50%"
+	),
 
-	h1(textOutput("header")),
+	h2(textOutput("header")),
+	textOutput("formula"),
+	fillRow(
+		plotlyOutput("time_series", width = "95%", height = "100%"),
+		plotlyOutput("scatter", width = "95%", height = "100%"),
+		height = "35%"
+	),
 	
-	fluidRow(
-		column(6, plotlyOutput("time_series")),
-		column(6, plotlyOutput("scatter"))
-	)
+	padding = c(0, 0, 0, 20)
 )
 
 server <- function(input, output) {
@@ -32,23 +39,50 @@ server <- function(input, output) {
 	fit <- read_csv("lm.csv")
 	metrics <- unique(fit$target)
 	players <- rev(unique(fit$player))
+	
+	my_plotly <- function(...) plot_ly(...) %>% 
+		config(displayModeBar = FALSE) %>% 
+		layout(
+			font = list(family = "Fira Code"),
+			xaxis = list(fixedrange = TRUE),
+			yaxis = list(fixedrange = TRUE)
+		)
 
 	output$heatmap <- renderPlotly({
 		fit %>% 
 			mutate(player = factor(player, levels = players)) %>% 
-			plot_ly(
+			my_plotly(
 				x = ~player, y = ~target, z = ~adj.r.squared,
+				text = ~sprintf("%s, %s: %.2f", player, target, adj.r.squared),
+				colors = colorRamp(c("#063672", "#ec0c1c")),
+				hoverinfo = "text",
 				source = "hm",
 				type = "heatmap"
-			)
+			) %>% 
+				hide_colorbar() %>% 
+				layout(
+					margin = list(l = 75, r = 50, b = 125),
+					xaxis = list(title = ""),
+					yaxis = list(title = "")
+				)
 	})
 	
 	output$header <- renderPrint({
 		s <- event_data("plotly_click", source = "hm")
 		if (length(s)) {
-			sprintf("%s | %s", s$x, s$y)
+			cat(s$x, "|", s$y)
 		} else {
-			"Click on the heatmap"
+			cat("click on the heatmap")
+		}
+	})
+	
+	output$formula <- renderPrint({
+		s <- event_data("plotly_click", source = "hm")
+		if (length(s)) {
+			fit %>% 
+				filter(player == s$x, target == s$y) %>% 
+				pull(model) %>% 
+				cat()
 		}
 	})
 	
@@ -79,13 +113,26 @@ server <- function(input, output) {
 		s <- event_data("plotly_click", source = "hm")
     	if (length(s)) {
 			my_fit(s$x, s$y) %>% 
+    			arrange(GameDate) %>% 
 				gather(key, value, -GameDate) %>% 
-				plot_ly(
+				my_plotly(
 					x = ~GameDate, y = ~value, color = ~key,
+					colors = c("#9b814e", "#d80919"),
+					line = list(dash = "dash", width = .5),
+					mode = "lines+markers",
 					type = "scatter"
-				)
+				) %>% 
+    				layout(
+    					hovermode = "x",
+    					legend = list(
+    						orientation = "h",
+    						x = .5, y = 1
+    					),
+						xaxis = list(title = ""),
+						yaxis = list(hoverformat = ".2f", range = c(-5, 5), title = "")
+    				)
     	} else {
-    		plotly_empty()
+    		plotly_empty() %>% config(displayModeBar = FALSE)
     	}
 	})
 	
@@ -93,12 +140,24 @@ server <- function(input, output) {
 		s <- event_data("plotly_click", source = "hm")
     	if (length(s)) {
 			my_fit(s$x, s$y) %>% 
-				plot_ly(
-					x = ~model, y = ~actual,
+				my_plotly(
+					x = ~model, y = ~actual, color = I("#063672"),
+					hoverinfo = "x+y",
 					type = "scatter"
-				)
+				) %>% 
+    				add_trace(
+    					x = c(-5, 5), y = c(-5, 5),
+    					line = list(color = "grey", dash = "dash", width = .5),
+						mode = "lines",
+						type = "scatter"
+    				) %>% 
+    				layout(
+    					showlegend = FALSE,
+    					xaxis = list(hoverformat = ".2f", range = c(-5, 5)),
+						yaxis = list(hoverformat = ".2f", range = c(-5, 5))
+    				)
     	} else {
-    		plotly_empty()
+    		plotly_empty() %>% config(displayModeBar = FALSE)
     	}
 	})
 }
