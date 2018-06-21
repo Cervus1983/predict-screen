f98e893644c1114380940a2db5bd6e4156013240
Mikhail Zhilkin <mikhail.zhilkin@kambi.com>
2018-06-21 10:40:49 +0200

    .

--- app.R
+++ app.R
@@ -1,33 +1,18 @@
 library(extrafont)
 library(ggthemes)
 library(MASS)
+library(plotly)
 library(shiny)
 library(tidyverse)
 
 ui <- fluidPage(
-	titlePanel("From Match to Screening"),
-	
-	plotOutput("heatmap", height = "800px"),
+	plotlyOutput("heatmap"),
 
+	h1(textOutput("header")),
+	
 	fluidRow(
-		column(
-			3,
-			selectInput(
-				"player",
-				label = NULL,
-				choices = players
-			),
-			selectInput(
-				"metric",
-				label = NULL,
-				choices = metrics
-			)
-		),
-		column(
-			9,
-			plotOutput("time_series"),
-			plotOutput("scatter")
-		)
+		column(6, plotlyOutput("time_series")),
+		column(6, plotlyOutput("scatter"))
 	)
 )
 
@@ -48,25 +33,23 @@ server <- function(input, output) {
 	metrics <- unique(fit$target)
 	players <- rev(unique(fit$player))
 
-	output$heatmap <- renderPlot({
+	output$heatmap <- renderPlotly({
 		fit %>% 
 			mutate(player = factor(player, levels = players)) %>% 
-			ggplot(aes(player, target)) +
-				coord_equal() +
-				geom_tile(aes(fill = adj.r.squared)) +
-				geom_text(aes(label = sprintf("%.2f", adj.r.squared))) +
-				labs(
-					title = "Adjusted R squared",
-					x = "<- fewer  |  more games played ->",
-					y = NULL
-				) +
-				scale_fill_gradient2() +
-				theme_tufte(ticks = FALSE) +
-				theme(
-					axis.text.x = element_text(angle = 90, hjust = 1),
-					legend.position = "none",
-					text = element_text(family = "Fira Code")
-				)
+			plot_ly(
+				x = ~player, y = ~target, z = ~adj.r.squared,
+				source = "hm",
+				type = "heatmap"
+			)
+	})
+	
+	output$header <- renderPrint({
+		s <- event_data("plotly_click", source = "hm")
+		if (length(s)) {
+			sprintf("%s | %s", s$x, s$y)
+		} else {
+			"Click on the heatmap"
+		}
 	})
 	
 	my_fit <- function(player, target) df %>% 
@@ -92,40 +75,31 @@ server <- function(input, output) {
 		) %>% 
 		rename_(actual = target)
 	
-	output$time_series <- renderPlot({
-		my_fit(input$player, input$metric) %>% 
-			gather(key, value, -GameDate) %>% 
-			ggplot(aes(GameDate, value, colour = key)) +
-				geom_line(alpha = .1) +
-				geom_point() +
-				labs(
-					x = NULL,
-					y = NULL
-				) +
-				scale_colour_manual(values = c("red", "black")) +
-				scale_y_continuous(limits = c(-3, 3)) +
-				theme_tufte(ticks = FALSE) +
-				theme(
-					legend.position = "top",
-					legend.title = element_blank(),
-					text = element_text(family = "Fira Code")
+	output$time_series <- renderPlotly({
+		s <- event_data("plotly_click", source = "hm")
+    	if (length(s)) {
+			my_fit(s$x, s$y) %>% 
+				gather(key, value, -GameDate) %>% 
+				plot_ly(
+					x = ~GameDate, y = ~value, color = ~key,
+					type = "scatter"
 				)
+    	} else {
+    		plotly_empty()
+    	}
 	})
 	
-	output$scatter <- renderPlot({
-		my_fit(input$player, input$target) %>% 
-			ggplot(aes(model, actual)) +
-				coord_fixed() +
-				geom_abline(slope = 1) +
-				geom_point() +
-				scale_x_continuous(limits = c(-3, 3)) +
-				scale_y_continuous(limits = c(-3, 3)) +
-				theme_tufte(ticks = FALSE) +
-				theme(
-					legend.position = "top",
-					legend.title = element_blank(),
-					text = element_text(family = "Fira Code")
+	output$scatter <- renderPlotly({
+		s <- event_data("plotly_click", source = "hm")
+    	if (length(s)) {
+			my_fit(s$x, s$y) %>% 
+				plot_ly(
+					x = ~model, y = ~actual,
+					type = "scatter"
 				)
+    	} else {
+    		plotly_empty()
+    	}
 	})
 }
 
